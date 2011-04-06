b819b28c4d346dd649a18e42ef1869626497e197
Jahroots <jahroots@gmail.com>
2011-04-06 22:30:34 +0200

    Add followers list

--- src/main/java/fr/jahroots/socializme/TestConnect.java
+++ src/main/java/fr/jahroots/socializme/TestConnect.java
@@ -1,17 +1,30 @@
 package fr.jahroots.socializme;
 
+import java.util.Collections;
 import java.util.List;
 
+import org.springframework.social.facebook.FacebookTemplate;
+import org.springframework.social.twitter.Tweet;
 import org.springframework.social.twitter.TwitterTemplate;
 
 public class TestConnect {
 
-    public static void main(String[] args) {
-        final TwitterTemplate twitterTemplate = new TwitterTemplate();
-        final List<String> friends = twitterTemplate.getFriends("chariotsolution");
+    private static final String USERNAME = "Jahroots972";
 
+	public static void main(String[] args) {
+        final TwitterTemplate twitterTemplate = new TwitterTemplate();
+        final List<String> friends = twitterTemplate.getFriends(USERNAME);
+        Collections.sort(friends);
+        System.out.println(USERNAME + " got " + friends.size() + " friends:");
         for (String friend : friends) {
-          System.out.println("Friend: " + friend);
+          System.out.println("\t - " + friend);
+        }
+        
+        final List<String> followers = twitterTemplate.getFollowers(USERNAME);
+        Collections.sort(followers);
+        System.out.println(USERNAME + " got " + friends.size() + " followers:");
+        for (String follower : followers) {
+          System.out.println("\t - " + follower);
         }
     }
 }
